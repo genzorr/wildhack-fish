52bad120d38573343a1d5d4c2d33acc3ffbdc2a6
algor <aleksgornostaev@gmail.com>
2021-12-04 10:59:35 +0300

    add template project with next js

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import Table from './Table'
+import React from 'react'
+class App extends React.Component {
+    render() {
+      return (
+        <div className="container">
+          <Table />
+        </div>
+      )
+    }
+  }
+
+  export default App
