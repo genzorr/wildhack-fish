26e9f15303f8edd9e3dc1259a8d77f7edeb4b7bf
Salakhov Ilia <salahov.ilia24@gmail.com>
2021-12-05 11:11:38 +0000

    fix sort

--- frontend/src/pages/customers.js
+++ frontend/src/pages/customers.js
@@ -3,48 +3,28 @@ import { Box, Container } from '@mui/material';
 import { CustomerListResults } from '../components/customer/customer-list-results';
 import { CustomerListToolbar } from '../components/customer/customer-list-toolbar';
 import { DashboardLayout } from '../components/dashboard-layout';
-import { customers } from '../__mocks__/customers';
-import { analyzed_sets_const } from 'src/__mocks__/analysed_sets';
 import { useState, useEffect } from 'react';
 const axios = require('axios').default;
 axios.defaults.baseURL = 'http://localhost:5000';
-
-const exampleAnalyzedSet = {
-  conf: 0.0,
-  count: 0,
-  date: "2021-12-05 12:11:41.457535",
-  name: "test",
-  path: "./images",
-  status: "loaded"
-}
 
 const Customers = () => {
 
   const [analyzed_sets, updateAnalyzedSets] = useState([])
 
 
-
   const onDbRequiresReload = () => {
     axios
     .get("/datasets")
     .then((response) => {
-      console.log(response)
-      const data = {};
+
       for (const value of response.data) {
         const valueDate = new Date(value.date);
-         const date = `${valueDate.getDate()}/${valueDate.getMonth()}/${valueDate.getFullYear()}`;
-         const dateObj = new Date(date);
 
-         if (!(date in data)) {
-           console.log(date, data)
-           data[date] = {
-             timestamp: dateObj.getTime(),
-             ...value   
-           }
-         }
+        value.date = valueDate.toISOString();
+        value.timestamp = valueDate.getTime();
       }
 
-      updateAnalyzedSets(Object.values(data).sort((a,b) => a.timestamp - b.timestamp))
+      updateAnalyzedSets(response.data.sort((a,b) => b.timestamp - a.timestamp))
     });
   }
 
